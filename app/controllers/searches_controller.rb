d5c78b0bb060628d1a7427e2122b44a46df54a46
Rachel Wyatt <rw126@evansville.edu>
2019-04-26 11:22:35 +0000

    Automatically backed up by Learn

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -9,6 +9,7 @@ class SearchesController < ApplicationController
       req.params['v'] = '20160201'
       req.params['near'] = params[:zipcode]
       req.params['query'] = 'coffee shop'
+      req.options.timeout = 0
     end
     body = JSON.parse(@resp.body)
      if @resp.success?
@@ -16,6 +17,9 @@ class SearchesController < ApplicationController
      else
        @error = body["meta"]["errorDetail"]
      end
+    rescue Faraday::ConnectionFailed
+      @error = "There was a timeout. Please try again."
+    end
     render 'search'
   end
 end
